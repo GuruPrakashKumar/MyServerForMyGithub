bbfe5e9985c860770fb60e5b698e75979b4ade6d
Guru Prakash Kumar <guruprakash745@gmail.com>
2023-08-19 09:55:11 +0530

    dsf

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ io.on('connection',(socket)=>{
   })
   socket.on("message",(msg)=>{
     console.log(msg)
+    let targetId = msg.targetId
+    if(clients[targetId]) clients[targetId].emit("message",msg)
   })
 })
 
