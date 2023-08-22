62b707a694c343ab2e521674b5728f4ddf6b3905
Guru Prakash Kumar <guruprakash745@gmail.com>
2023-08-22 07:52:41 +0530

    sd

--- routes/chat_routes.js
+++ routes/chat_routes.js
@@ -3,6 +3,21 @@ const router = express.Router();
 const authRoutes = require('./auth_routes'); // Assuming this is your auth middleware
 const userChatModel = require('../models/chat_model'); // Import the chat model
 
+router.get('/getAllTargetEmails',authRoutes.verifyToken,async (req,resp)=>{
+  try {
+    const userChat = await userChatModel.findOne({
+      email:req.authData.user.email
+    })
+    if (!userChat) {
+      return resp.status(404).json({ message: 'User chat history not found' });
+    }
+    const targetEmails = userChat.chats.map(chat => chat.targetEmail);
+    
+  } catch (error) {
+    resp.status(500).json({message: 'server error'})
+  }
+})
+
 router.post('/getChatHistory', authRoutes.verifyToken, async (req, resp) => {
   try {
     const userChat = await userChatModel.findOne({
