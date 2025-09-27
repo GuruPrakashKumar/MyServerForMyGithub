e890ad0ff1604e7b7ab5e0ade0e6ac1b24584760
Guru Prakash Kumar <guruprakash745@gmail.com>
2025-09-27 12:20:14 +0530

    added health check

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const mongodbAtlasDatabaseUrl = process.env.MONGODB_ATLAS_DATABASE_LINK;
 const http = require('http')
 const server = http.createServer(app)
 const io = require("socket.io")(server)
-
+const pingInterval = 1 * 60 * 1000; // 1 minutes
 const socketController = require('./controllers/socketController');
 socketController(io);
 
@@ -21,6 +21,23 @@ app.use(bodyParser.json());
 
 const routesController = require('./controllers/routesController');
 app.use('/', routesController);
+
+//to keep server alive
+const pingSelf = async () => {
+  try {
+    await axios.get(`${process.env.SERVER_URL}/health-check`);
+    // await axios.get('http://localhost:8000/health-check');
+  } catch (error) {
+    console.error('Self-ping failed:', error.message);
+  }
+};
+
+// Start the interval
+setInterval(pingSelf, pingInterval);
+
+app.get('/health-check', (req, res) => {
+  res.status(200).send('OK');
+});
 
 server.listen(port, '0.0.0.0', () => {
   console.log("Server connected to Socket.IO");
